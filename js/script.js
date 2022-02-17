259977aca4fa4aa980d53a2414eef954f9fb698f
Daniel <daniel.bnh13gmail.com>
2022-02-17 15:06:42 -0300

    js dos inputs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const QUIZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz";
-
+let tituloQuizz
+let url
+let quantidadePerguntas
+let quantidadeNiveis
 function obterQuizzes() {
     const promise = axios.get(`${QUIZZ_API}/quizzes`);
     promise.then(resposta => {
@@ -11,26 +14,54 @@ function obterQuizzes() {
       alert("Xiii! Deu ruim na hora de receber mensagens!");
     })
   }
-  function tela1(){
+  function tela2(){
     const tela1 = document.querySelector('.tela1')
     tela1.classList.add('esconderTela')
     const tela2 = document.querySelector('.tela2')
     tela2.classList.remove('esconderTela')
   }
-  function tela2(){
+  function tela3(){
     const tela1 = document.querySelector('.tela1')
     tela1.classList.add('esconderTela')
     const tela3 = document.querySelector('.tela3')
     tela3.classList.remove('esconderTela')
+    
   }
   function tela32(){
+    tituloQuizz =document.querySelector(".ip1").value;
+    url =document.querySelector(".ip2").value;
+    quantidadePerguntas =document.querySelector(".ip3").value;
+    quantidadeNiveis =document.querySelector(".ip4").value;
     
-    const tela3 = document.querySelector('git.tela31')
-    tela3.classList.add('esconderTela')
-    const tela32 = document.querySelector('.tela32')
-    tela32.classList.remove('esconderTela')
+    if(tituloQuizz.length > 65){
+      alert('Titulo no máximo 65 caracteres')
+    }if(tituloQuizz.length < 20){
+      alert('Titulo deve ter no mínimo 20 caracteres')
+    }
+    if(quantidadePerguntas >3){
+      alert('quantidade de perguntas: no mínimo 3 perguntas')
+    }
+    if(quantidadeNiveis >2){
+      alert('Quantidade de níveis: no mínimo 2 níveis')
+    }
+    console.log(quantidadeNiveis <=2)
+    console.log(quantidadeNiveis)
+    if(tituloQuizz !=='' && url !=='' && quantidadePerguntas !=='' && quantidadeNiveis !=='' && 65>=tituloQuizz.length && tituloQuizz.length >=20 && quantidadePerguntas <=3 && quantidadeNiveis <=2){
+      const tela3 = document.querySelector('.tela3')
+      tela3.classList.add('esconderTela')
+      const tela32 = document.querySelector('.tela32 ')
+      tela32.classList.remove('esconderTela')
+    }else(alert('erro'))
+   
 
-  }
+ }
+ function tela33(){  
+   alert(tituloQuizz)
+  const tela32 = document.querySelector('.tela32 ')
+  tela32.classList.add('esconderTela')
+  const tela33 = document.querySelector('.tela33 ')
+  tela33.classList.remove('esconderTela')
+}
   
   function renderizarQuizzes(quizzes) {
     const mostrarQuizz = document.querySelector(".corpo-quizzes");
@@ -42,7 +73,7 @@ function obterQuizzes() {
     
    
         mostrarQuizz.innerHTML +=  `
-        <div onclick="tela1()" class="container-quizz">
+        <div onclick="tela2()" class="container-quizz">
           <img class="imagem-quizz" src="${imagem}">
           <p class="texto-quizz">${titulo}</p>
         </div>
