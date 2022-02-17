f5f4a8aa8278680718c21782d23836882fc443a3
Wallyson <wallysom2@gmail.com>
2022-02-17 15:59:04 -0300

    resolvendo conflitos

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let tituloQuizz
 let url
 let quantidadePerguntas
 let quantidadeNiveis
-const gradiente = "background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65.62%, rgba(0, 0, 0, 0.8) 100%)"
-    function obterQuizzes() {
+   
+function obterQuizzes() {
     const promise = axios.get(`${QUIZZ_API}/quizzes`);
     promise.then(resposta => {
       console.log(resposta.data);
@@ -15,7 +15,59 @@ const gradiente = "background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%
       alert("Xiii! Deu ruim na hora de receber mensagens!");
     })
   }
-  function tela2(){
+  function tela2(){  }
+    
+    function obterQuizzes1() {
+    const promise = axios.get(`${QUIZZ_API}`);
+    promise.then(resposta => {
+      console.log(resposta.data);
+      renderizarQuizzes1(resposta.data);
+    });
+    promise.catch( erro => {
+      console.error(erro.response);
+      alert("Xiii! Deu ruim na hora de receber mensagens!");
+    })
+  }
+
+
+  
+  function renderizarQuizzes(quizzes) {
+    const mostrarQuizz = document.querySelector(".corpo-quizzes");
+    mostrarQuizz.innerHTML = "";
+    quizzes.forEach( quizz => {
+        const id = quizz.id;
+        const titulo = quizz.title;
+        const imagem = quizz.image;
+    
+   
+        mostrarQuizz.innerHTML +=  `
+        <div onclick="tela1()" class="container-quizz">
+          <img class="imagem-quizz" src="${imagem}">
+          <p class="texto-quizz">${titulo}</p>
+        </div>
+          `
+
+    }) }
+
+    function renderizarQuizzes1 (quizzes) {
+      const mostrarQuizz = document.querySelector(".quizz-aberto");
+      mostrarQuizz.innerHTML = "";
+      quizzes.forEach( quizz => {
+          const id = quizz.id;
+          const titulo = quizz.title;
+          const imagem = quizz.image;
+      
+     
+          mostrarQuizz.innerHTML +=  `
+          <div onclick="tela1()" class="container-quizz">
+            <img class="imagem-quizz" src="${imagem}">
+            <p class="texto-quizz">${titulo}</p>
+          </div>
+            `
+  
+      }) } 
+      
+      function tela1(){
     const tela1 = document.querySelector('.tela1')
     tela1.classList.add('esconderTela')
     const tela2 = document.querySelector('.tela2')
